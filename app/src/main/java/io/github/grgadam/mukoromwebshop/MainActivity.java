553880a77ea07cf0af0432f5eaeccd70df45516c
GrGadam <grginsteam@gmail.com>
2024-03-10 17:21:02 +0100

    upload

--- app/src/main/java/io/github/grgadam/mukoromwebshop/MainActivity.java
+++ app/src/main/java/io/github/grgadam/mukoromwebshop/MainActivity.java
@@ -2,7 +2,9 @@ package io.github.grgadam.mukoromwebshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void showRegisterActivity(View view) {
+        Intent showRegisterIntent = new Intent(this, RegsiterActivity.class);
+        startActivity(showRegisterIntent);
     }
 }
