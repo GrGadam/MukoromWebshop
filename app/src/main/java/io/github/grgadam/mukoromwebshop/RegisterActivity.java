43bae7f4c3759d27f1d7665272794948ffb35309
GrGadam <grginsteam@gmail.com>
2024-03-11 15:31:54 +0100

    Finish register page

--- app/src/main/java/io/github/grgadam/mukoromwebshop/RegisterActivity.java
+++ app/src/main/java/io/github/grgadam/mukoromwebshop/RegisterActivity.java
@@ -1,7 +1,11 @@
 package io.github.grgadam.mukoromwebshop;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.widget.AppCompatButton;
 
 public class RegisterActivity extends Activity {
 
@@ -9,6 +13,12 @@ public class RegisterActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        AppCompatButton registerButton = findViewById(R.id.loginButton);
+        registerButton.setOnClickListener(this::showLoginActivity);
+    }
+
+    private void showLoginActivity(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
     }
 
 }
