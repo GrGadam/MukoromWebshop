bfdadd9b4eeda35bab3bfe39914627406400fcd7
GrGadam <grginsteam@gmail.com>
2024-03-11 15:18:52 +0100

    Refactoring, autoHint fix and add onClickListener

--- app/src/main/java/io/github/grgadam/mukoromwebshop/LoginActivity.java
+++ app/src/main/java/io/github/grgadam/mukoromwebshop/LoginActivity.java
@@ -1,17 +1,25 @@
 package io.github.grgadam.mukoromwebshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        AppCompatButton registerButton = findViewById(R.id.regsiterButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRegisterActivity(v);
+            }
+        });
     }
 
     public void showRegisterActivity(View view) {
