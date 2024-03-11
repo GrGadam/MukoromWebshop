7cfc2481cf59a596de385b80800fd6fc80e943be
GrGadam <grginsteam@gmail.com>
2024-03-11 16:30:20 +0100

    upload

--- app/src/main/java/io/github/grgadam/mukoromwebshop/LoginActivity.java
+++ app/src/main/java/io/github/grgadam/mukoromwebshop/LoginActivity.java
@@ -1,23 +1,59 @@
 package io.github.grgadam.mukoromwebshop;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText usernameEditText;
+    EditText passwordEditText;
+    AppCompatButton loginButton;
+    AppCompatButton registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        AppCompatButton registerButton = findViewById(R.id.regsiterButton);
+
+        //get elements
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.regsiterButton);
+
+        //register Button send to register Activity
         registerButton.setOnClickListener(this::showRegisterActivity);
+
+        //login method on login Button click
+        loginButton.setOnClickListener(this::login);
     }
 
     public void showRegisterActivity(View view) {
-        startActivity(new Intent(this, RegisterActivity.class));
+        Intent showRegisterIntent = new Intent(this, RegisterActivity.class);
+
+        if (!usernameEditText.getText().toString().isEmpty()) {
+            showRegisterIntent.putExtra("username", usernameEditText.getText().toString());
+        }
+
+        startActivity(showRegisterIntent);
+    }
+
+    private void login(View view) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
--- app/src/main/java/io/github/grgadam/mukoromwebshop/RegisterActivity.java
+++ app/src/main/java/io/github/grgadam/mukoromwebshop/RegisterActivity.java
@@ -4,21 +4,52 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.widget.AppCompatButton;
 
 public class RegisterActivity extends Activity {
 
+    EditText usernameEditText;
+    EditText passwordEditText;
+    EditText passwordAgainEditText;
+    EditText emailEditText;
+    AppCompatButton registerButton;
+    AppCompatButton loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        AppCompatButton registerButton = findViewById(R.id.loginButton);
-        registerButton.setOnClickListener(this::showLoginActivity);
+
+        //get elements
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        passwordAgainEditText = findViewById(R.id.passwordAgainEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        registerButton = findViewById(R.id.regsiterButton);
+        loginButton = findViewById(R.id.loginButton);
+
+        //login Button send to Login Activity
+        loginButton.setOnClickListener(this::showLoginActivity);
+
+        //register on register Button click
+        registerButton.setOnClickListener(this::register);
+
+        //autofill username
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            usernameEditText.setText(extras.getString("username"));
+        }
+
     }
 
     private void showLoginActivity(View view) {
         startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    private void register(View view) {
+        //TODO
     }
 
 }
