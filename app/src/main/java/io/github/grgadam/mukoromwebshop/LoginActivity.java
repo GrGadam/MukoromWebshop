44da5a519dacea404cfc55562db594366fa00661
GrGadam <grginsteam@gmail.com>
2024-05-09 21:19:18 +0200

    add orientations

--- app/src/main/java/io/github/grgadam/mukoromwebshop/LoginActivity.java
+++ app/src/main/java/io/github/grgadam/mukoromwebshop/LoginActivity.java
@@ -3,6 +3,7 @@ package io.github.grgadam.mukoromwebshop;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +19,18 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //ertesitessav es navbar eltuntetese
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         setContentView(R.layout.activity_login);
 
         //get elements
--- app/src/main/java/io/github/grgadam/mukoromwebshop/RegisterActivity.java
+++ app/src/main/java/io/github/grgadam/mukoromwebshop/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 
 import androidx.appcompat.widget.AppCompatButton;
@@ -24,6 +25,18 @@ public class RegisterActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //ertesitessav es navbar eltuntetese
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         setContentView(R.layout.activity_register);
 
         //get elements
